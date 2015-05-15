43451fe1152743286e601899e908825ba0519267
m_nishida <sbi16tmg3ishropi7171@i.softbank.jp>
2015-05-15 22:34:54 +0900

    a

---
+++ ConsoleApplication1/ConsoleApplication1/Class1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Class_Invisible
+
+    Structure Structure_Log
+        Public ReadOnly log As String
+        Public ReadOnly time As Date
+
+        Sub New(s As String)
+            log = s
+            time = Date.Now
+        End Sub
+
+        Sub WriteLog()
+            Console.WriteLine("Invisible")
+        End Sub
+    End Structure
+
+End Class
---
+++ ConsoleApplication1/ConsoleApplication1/Module1.vb
@@ -0,0 +1,44 @@
+﻿Module Module1
+
+    Sub Main()
+        Class_Normal.GetValue.WriteLog()
+        Class_Invisible.GetValue.WriteLog()
+    End Sub
+
+    Structure Structure_Log
+        Public ReadOnly log As String
+        Public ReadOnly time As Date
+
+        Sub New(s As String)
+            log = s
+            time = Date.Now
+        End Sub
+
+        Sub WriteLog()
+            Console.WriteLine(time.ToString + log)
+        End Sub
+    End Structure
+
+End Module
+
+Partial Class Class_Normal
+
+    Shared t As New Structure_Log("Class_Normal")
+
+    Shared Function GetValue() As Structure_Log
+        Return t
+    End Function
+
+End Class
+
+Partial Class Class_Invisible
+
+    Shared t As New Structure_Log("Class_Invisible")
+
+    Shared Function GetValue() As Structure_Log
+        Return t
+    End Function
+
+End Class
+
+
